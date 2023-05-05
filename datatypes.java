596dc8c36e325225d8e65c7a70f7c2c177a1db47
BANZO <aditya.panwar.2208@gmail.com>
2023-05-05 20:13:15 +0530

    datatypes

---
+++ datatypes.java
@@ -0,0 +1,17 @@
+class datatypes {
+    public static void main(String A[]) {
+        int num1 = 655485;
+        long num2 = 67899033604l;
+        short num3 = 3356;
+        byte num4 = 127;
+
+        float num5 = 55.9f;
+        double num6 = 55.9;
+
+        char num7 = 'A';
+        
+        boolean num8 = true;
+
+        System.out.println(num8);
+    }
+}
