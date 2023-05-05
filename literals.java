298e567d460e25e39e91edf4dfe97ac4b9ca73b0
BANZO <aditya.panwar.2208@gmail.com>
2023-05-05 20:44:14 +0530

    literals1

---
+++ literals.java
@@ -0,0 +1,15 @@
+public class literals {
+    public static void main(String A[]) {
+        // Literals: 
+        // Integer Literal
+        // Character Literal
+        // Boolean Literal
+        // String Literal
+
+        int deci = 26;
+        int by = 0b110; // 6
+        int ot = 067; // 55
+        System.out.println(by);
+    
+    }
+}
