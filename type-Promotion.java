f583e6051770b199f5083b6859512e518580b22f
BANZO <aditya.panwar.2208@gmail.com>
2023-05-06 07:24:32 +0530

    Type promotion

---
+++ type-Promotion.java
@@ -0,0 +1,15 @@
+class typePromotion {
+    public static void main(String[] args) {
+        // int / int = int 
+        // int / float = float
+        // int * float = float
+        // short * short = int
+        // short * int = int
+        // short * long = long
+        // byte * byte = int
+        System.out.println(5.2/3);
+        byte b=120; 
+        byte c=120;
+        System.out.println(b*c); //14400
+    }
+}
