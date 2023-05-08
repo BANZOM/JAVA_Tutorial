b44cf5d74e2179e82486fa4d32351fba78def385
BANZO <aditya.panwar.2208@gmail.com>
2023-05-08 14:44:50 +0530

    Modified

--- logicalO.java
+++ logicalO.java
@@ -1,3 +1,5 @@
+import javax.print.event.PrintEvent;
+
 public class logicalO {
     public static void main(String[] args) {
         System.out.println(true & true);
@@ -10,5 +12,9 @@ public class logicalO {
         System.out.println(!false);
     
         // you can use either && or &, both will work the same
+        System.out.println("With Relational operators: ");
+        System.out.println(2>3 | 2>1);
+        System.out.println(2>3 & 2>1);
+
     }
 }
