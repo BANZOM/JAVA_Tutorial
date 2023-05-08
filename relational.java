e9ddcfdf48d0e31b872fb782b6ffab501c0e2f39
BANZO <aditya.panwar.2208@gmail.com>
2023-05-08 14:25:51 +0530

    rename for bugfix

---
+++ relational.java
@@ -0,0 +1,12 @@
+/**
+ * relational-operator
+ */
+public class relational {
+    public static void main(String A[]) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        boolean 
+    }
+}
