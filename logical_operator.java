7bfb9bf5ebda6935d85c497f0a53a64beafeba2c
BANZO <aditya.panwar.2208@gmail.com>
2023-05-08 14:51:24 +0530

    Modified

--- logical_operator.java
+++ logical_operator.java
@@ -11,11 +11,11 @@ public class logical_operator {
         System.out.println(!true);
         System.out.println(!false);
     
-        // you can use either && or &, both will work the same
+        // Logical AND --> && ; Bitwise AND --> &, both will work the same with booleans
         System.out.println("With Relational operators: ");
         System.out.println(2>3 | 2>1);
         System.out.println(2>3 & 2>1);
 
-        // 
+
     }
 }
