91896a10b0e6b06e48dc0e1f021d8e34afa843c0
BANZO <aditya.panwar.2208@gmail.com>
2023-05-08 14:28:58 +0530

    updates

--- relational.java
+++ relational.java
@@ -5,8 +5,20 @@ public class relational {
     public static void main(String A[]) {
         int a = 10;
         int b = 20;
-        int c = 30;
+        int c = 20;
 
-        boolean 
+        // these relational operators return boolean value
+        boolean k = a<b;
+        boolean l = a>b;
+        boolean m = a<=b;
+        boolean n = b==c;
+        boolean o = b!=c;
+
+        System.out.println(k);
+        System.out.println(l);
+        System.out.println(m);
+        System.out.println(n);
+        System.out.println(o);
+
     }
 }
