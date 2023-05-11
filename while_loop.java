b9a306d35dc014e1a3e63c57a6285d7a01460b7f
BANZO <aditya.panwar.2208@gmail.com>
2023-05-11 21:30:29 +0530

    loop extended

--- while_loop.java
+++ while_loop.java
@@ -2,8 +2,14 @@ public class while_loop {
     public static void main(String[] args) {
         
         int a = 1;
+
         while (a <= 10) {
-            System.out.println(a);
+            int b = 1;
+            while (b <= a) {
+                System.out.print("b");
+                b++;
+            } 
+            System.out.println();
             a++;
         }
 
