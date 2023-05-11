d76d8167c4f58af3a5f90bf69a373684dbe8b4c4
BANZO <aditya.panwar.2208@gmail.com>
2023-05-11 21:23:59 +0530

    Added While loop basics

---
+++ while_loop.java
@@ -0,0 +1,12 @@
+public class while_loop {
+    public static void main(String[] args) {
+        boolean S = true;
+
+        while (S) {
+            System.out.println("Hello");
+            S = false;
+        }
+
+        System.out.println(S);
+    }
+}
