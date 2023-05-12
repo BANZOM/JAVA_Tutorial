12180fc78fbdaf6d881af041aa48921e90ac14ae
BANZO <aditya.panwar.2208@gmail.com>
2023-05-12 22:16:43 +0530

    Comments

---
+++ comments.java
@@ -0,0 +1,20 @@
+public class comments {
+    public static void main(String[] A) {
+
+        // When comments are short we use the single-line syntax: //
+
+        // When comments are long we use the multi-line syntax: /* and */
+        /*
+         * We chose to store information across multiple databases to
+         * minimize the possibility of data loss. We'll need to be careful
+         * to make sure it does not go out of sync!
+         */
+
+        /**
+         * Another type of commenting option is the Javadoc comment which is represented by 
+         * Javadoc comments are used to create documentation for APIs (Application Programming Interfaces). 
+         * When writing Javadoc comments, remember that they will eventually be used in the documentation that 
+         * your users might read, so make sure to be especially thoughtful when writing these comments.
+         */
+    }
+}
