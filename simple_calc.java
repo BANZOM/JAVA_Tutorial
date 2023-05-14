15b4ca2d89ffce00dd9bfdb4ebd0809afc06cf78
BANZO <aditya.panwar.2208@gmail.com>
2023-05-14 13:44:19 +0530

    Modified

--- simple_calc.java
+++ simple_calc.java
@@ -1,8 +1,11 @@
 public class simple_calc {
     public static void main(String[] args) {
-
+        int n;
         System.out.println("CALCULATOR MENU");
-
         System.out.println("1. Additon\n2. Subtraction\n3. Multiplication\n4. Division");
+
+        do {
+            n = Integer.parseInt(System.console().readLine());
+        } while (n != 5);
     }
 }
