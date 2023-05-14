fe3da64859b44f7dc48b1b5c0904c6067a2dbdc9
BANZO <aditya.panwar.2208@gmail.com>
2023-05-14 07:54:18 +0530

    string object

---
+++ stringinit.java
@@ -0,0 +1,15 @@
+public class stringinit {
+    public static void main(String[] args) {
+
+        // string object
+        String s = "hello";
+        // string new object
+        String s1 = new String("hello");
+        
+        // printing
+        System.out.println(s);
+        System.out.println(s1);
+
+
+    }
+}
