01493acf0b40255caa93d3b3c23e854b6ec82bb0
BANZO <aditya.panwar.2208@gmail.com>
2023-05-14 13:56:01 +0530

    Modified

--- simple_calc.java
+++ simple_calc.java
@@ -1,14 +1,29 @@
+class calculations{
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int sub(int a, int b) {
+        return a - b;
+    }
+    public int mul(int a, int b) {
+        return a * b;
+    }
+    public double div(int a, int b) {
+        return a / b;
+    }
+}
+
 public class simple_calc {
     public static void main(String[] args) {
         int n1 = 2;
         int n2 = 22;
-
         int result;
 
-        
-        
+        calculations calc = new calculations();
 
+        result = calc.add(n1, n2);
+        System.out.print("Addition:");
+        System.out.println(result);
 
-        
     }
 }
