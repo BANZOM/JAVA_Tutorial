9ade0e0d0ffb16f0cb7c5e44453e8b3cc3e50d5a
BANZO <aditya.panwar.2208@gmail.com>
2023-05-14 13:40:59 +0530

    Initialized and Added Menu

---
+++ simple_calc.java
@@ -0,0 +1,8 @@
+public class simple_calc {
+    public static void main(String[] args) {
+
+        System.out.println("CALCULATOR MENU");
+
+        System.out.println("1. Additon\n2. Subtraction\n3. Multiplication\n4. Division");
+    }
+}
