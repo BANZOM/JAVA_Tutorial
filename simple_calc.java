ad95b5a46c73824f4b36d9daad3dea8e6fbc993a
BANZO <aditya.panwar.2208@gmail.com>
2023-05-14 13:56:39 +0530

    Modified: addition subtraction

--- simple_calc.java
+++ simple_calc.java
@@ -25,5 +25,9 @@ public class simple_calc {
         System.out.print("Addition:");
         System.out.println(result);
 
+        result = calc.sub(n1, n2);
+        System.out.print("Subtraction:");
+        System.out.println(result);
+
     }
 }
