5f7af431ccfc2a4d3100a8d6434e90a7718eb2d1
BANZO <aditya.panwar.2208@gmail.com>
2023-05-16 03:58:14 +0530

    Dynamic array

--- array.java
+++ array.java
@@ -1,8 +1,20 @@
 public class array {
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5};
+
+        System.out.println("Static: ");
         for (int i = 0; i < 5; i++) {
             System.out.println(arr[i]);
+        }
+
+        System.out.println("Dynamic: ");
+        int arr2[] = new int[5];
+        for (int i = 0; i < 5; i++) {
+            arr2[i] = i+5;
+        }
+
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.println(arr2[i]);
         }
     }
 }
