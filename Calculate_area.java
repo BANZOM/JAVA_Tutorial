c5a45496ef53cf8334fa5fc9189ef4ae72f04ff3
BANZO <aditya.panwar.2208@gmail.com>
2023-05-16 16:05:31 +0530

    with bug

---
+++ Calculate_area.java
@@ -0,0 +1,21 @@
+class Rectangle{
+    private int length, breadth;
+    Rectangle(int x,int y) {
+        length = x;
+        breadth = y;
+    }
+    public int calculate {
+        int area = length * breadth;
+        return area;
+    }
+}
+
+public class Calculate_area {
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle(5,6);
+        int area = r.calculate();
+        System.out.println();
+    }
+}
+
+
