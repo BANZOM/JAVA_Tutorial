1d67a3d8a955746c446fbc3fcf71298de9223e54
BANZO <aditya.panwar.2208@gmail.com>
2023-05-16 16:27:37 +0530

    without bug

--- Calculate_area.java
+++ Calculate_area.java
@@ -1,10 +1,12 @@
-class Rectangle{
+class Rectangle {
     private int length, breadth;
-    Rectangle(int x,int y) {
+
+    Rectangle(int x, int y) {
         length = x;
         breadth = y;
     }
-    public int calculate {
+
+    public int calculate() {
         int area = length * breadth;
         return area;
     }
@@ -12,10 +14,11 @@ class Rectangle{
 
 public class Calculate_area {
     public static void main(String[] args) {
-        Rectangle r = new Rectangle(5,6);
+        Rectangle r = new Rectangle(5, 6);
         int area = r.calculate();
-        System.out.println();
+        System.out.println("Area of rectangle is: " + area);
     }
 }
 
 
+// Implemented Encapsulation, Abstraction, method, object
