3f8c7e61b7214eb174cd1a2b922f9e58d201ad8f
BANZO <aditya.panwar.2208@gmail.com>
2023-05-16 20:58:01 +0530

    Assigning random values

--- Array2.java
+++ Array2.java
@@ -1,6 +1,13 @@
 class Array_2D {
     public int[][] Integer_array(int row, int col) {
         int arr[][] = new int[row][col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                arr[i][j] = (int)(Math.random() * 10);
+            }
+        }
+
         return arr;
     }
 }
@@ -9,10 +16,12 @@ class Array_2D {
 public class Array2 {
     public static void main(String[] args) {
         Array_2D a = new Array_2D();
-        int[][] b = a.Integer_array(2, 3);
+        int row = 4;
+        int col = 5;
+        int[][] b = a.Integer_array(row, col);
 
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 System.out.print(b[i][j]);
                 System.out.print(",");
             }
