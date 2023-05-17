ee9ff10071d48c290d7fdf8d1224db1b58f01ecd
BANZO <aditya.panwar.2208@gmail.com>
2023-05-17 23:15:17 +0530

    Added comments and docstrings

--- Array_object.java
+++ Array_object.java
@@ -3,8 +3,16 @@ class Student {
     int age;
 }
 
+/**
+ * A class to demonstrate the use of array of objects.
+ */
 public class Array_object {
+    /**
+     * The main method.
+     * @param args The command-line arguments.
+     */
     public static void main(String[] args) {
+        // Creating three student objects
         Student s1 = new Student();
         s1.name = "Aditya";
         s1.age = 19;
@@ -17,11 +25,13 @@ public class Array_object {
         s3.name = "Banzo";
         s3.age = 21;
 
+        // Creating an array of student objects
         Student[] students = new Student[3];
         students[0] = s1;
         students[1] = s2;
         students[2] = s3;
 
+        // Iterating through the array and printing the details of each student
         for (int i = 0; i < students.length; i++) {
             System.out.println(students[i].name + " " + students[i].age);
         }
