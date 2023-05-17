afcd8349f1de9994d91847e30925ed106b673f19
BANZO <aditya.panwar.2208@gmail.com>
2023-05-17 23:10:29 +0530

    file initialized

---
+++ Array_object.java
@@ -0,0 +1,11 @@
+class Student {
+    String name;
+    int age;
+}
+
+public class Array_object {
+    public static void main(String[] args) {
+        
+        Student[] st1 = new Student[4];
+    }
+}
