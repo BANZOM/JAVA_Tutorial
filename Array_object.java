9a0db5a774c8d5a4d611b880f73b6ddf223025b7
BANZO <aditya.panwar.2208@gmail.com>
2023-05-17 23:10:48 +0530

    created objects

--- Array_object.java
+++ Array_object.java
@@ -5,7 +5,18 @@ class Student {
 
 public class Array_object {
     public static void main(String[] args) {
-        
-        Student[] st1 = new Student[4];
+        Student s1 = new Student();
+        s1.name = "Aditya";
+        s1.age = 19;
+
+        Student s2 = new Student();
+        s2.name = "Panwar";
+        s2.age = 20;
+
+        Student s3 = new Student();
+        s3.name = "Banzo";
+        s3.age = 21;
+
+        Student[] students = new Student[4];
     }
 }
