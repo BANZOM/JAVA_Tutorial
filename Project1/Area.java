492d8dda866cbb6aca822757435075063ac2c0e4
BANZO <aditya.panwar.2208@gmail.com>
2023-05-18 15:31:11 +0530

    Understanding constructors

---
+++ Project1/Area.java
@@ -0,0 +1,17 @@
+package Project1;
+
+public class Area {
+    private length, breadth;
+
+    // constructor
+    Area (int l, int b) {
+        this.length = l;
+        this.breadth = b;
+    }
+
+    // default constructor
+    Area() {
+        
+    }
+}
+
---
+++ Project1/demo.java
@@ -0,0 +1,7 @@
+package Project1;
+
+public class demo {
+    public static void main(String[] args) {
+        Area a = new Area(3,4);
+    }
+}
