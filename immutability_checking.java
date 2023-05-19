63a2d666a895396ab5249f490ec01bdc31dd9a57
BANZO <aditya.panwar.2208@gmail.com>
2023-05-19 20:49:53 +0530

    Immutable string, String Constant pool

---
+++ immutability_checking.java
@@ -0,0 +1,16 @@
+public class immutability_checking {
+    public static void main(String[] args) {
+        String s1 = "hello";
+        String s2 = "hello";
+        String s3 = "Hello";
+
+        System.out.println(s1 == s2); // true
+        System.out.println(s1 == s3); // false
+
+        s3 = "hello";
+        System.out.println(s1 == s3); // true
+
+        // here the object "Hello" will goes under Garbage collector from the string constant pool;
+
+    }
+}
