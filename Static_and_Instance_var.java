2d20f94ba046e345662ff8fb895f1caf291d86ee
BANZO <aditya.panwar.2208@gmail.com>
2023-05-20 07:03:13 +0530

    Added static variable

--- Static_and_Instance_var.java
+++ Static_and_Instance_var.java
@@ -2,7 +2,7 @@ class Cars{
     String name;
     int seats;
     int maxSpeed;
-    int tyres;
+    static int tyres; // observing all cars have 4 tyres, making it static
     int noOfLights;
 
     void show() {
@@ -15,16 +15,16 @@ public class Static_and_Instance_var {
         Cars car1 = new Cars();
         Cars car2 = new Cars();
 
+        Cars.tyres = 4;
+
         car1.name = "BMW";
         car1.seats = 4;
         car1.maxSpeed = 200;
-        car1.tyres = 4;
         car1.noOfLights = 4;
 
         car2.name = "Porsche";
         car2.seats = 2;
         car2.maxSpeed = 250;
-        car2.tyres = 4;
         car2.noOfLights = 8;
 
         car1.show();
