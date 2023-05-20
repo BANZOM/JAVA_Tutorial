281d226a68512c5399dd51ef194ce0b8601b2b13
BANZO <aditya.panwar.2208@gmail.com>
2023-05-20 06:37:25 +0530

    Mutable Strings Operations- insert,append

--- mutable_strings.java
+++ mutable_strings.java
@@ -2,5 +2,20 @@ public class mutable_strings {
     public static void main(String[] args) {
         
         // Mutable Strings are formed by StringBuffer and StringBuilder
+        StringBuilder sb1 = new StringBuilder("Hello");
+        StringBuilder sb2 = new StringBuilder();
+
+        System.out.println(sb2.capacity());
+
+        // Operations:
+        System.out.println(sb1.hashCode());
+        sb1.append(" is the instance");
+
+        System.out.println(sb1.hashCode());
+        System.out.println(sb1);
+
+        // Insert
+        sb1.insert(5, " World");
+        System.out.println(sb1);
     }
 }
