9b1f274ff52652672cc1eeda469bf4eaece141ab
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-22 09:38:13 +0530

    basic info

--- ThisAndSuper.java
+++ ThisAndSuper.java
@@ -1,6 +1,20 @@
 public class ThisAndSuper {
     // Understanding the this() an super() methods 
     public static void main(String[] args) {
+
+        // this() method
+        // this() method is used to call the constructor of the same class
+        // this() method should be the first statement in the constructor
+        // this() method can be used only in constructors
+        // this() method can be used only once
+
+        // super() method
+        // super() method is used to call the constructor of the parent class
+        // super() method should be the first statement in the constructor
+        // super() method can be used only in constructors
+        // super() method can be used only once
+
+        
         
     }
 }
