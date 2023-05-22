cd8f3f08dfdad697594e9d32b82c86887280a640
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-22 12:21:59 +0530

    Basic Calc without Inherit

---
+++ InheritanceImplementation/Inheritance.java
@@ -0,0 +1,10 @@
+package InheritanceImplementation;
+
+
+public class Inheritance {
+    public static void main(String[] args) {
+        Calc c = new Calc();
+        System.out.println(c.add(1, 2));
+        System.out.println(c.subtract(1, 2));
+    }
+}
