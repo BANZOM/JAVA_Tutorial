31a42675152236ceea2dd2f57a63583b1f963794
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-22 16:45:42 +0530

    about packages: typed

--- UnderstandingPackage/UnderstandingPackage.java
+++ UnderstandingPackage/UnderstandingPackage.java
@@ -4,5 +4,15 @@ import UnderstandingPackage.Package1.Print;
 public class UnderstandingPackage {
     public static void main(String[] args) {
         Print p = new Print();
+
+    // About Packages:
+    // A package is a collection of classes and interfaces.
+    // A package can contain other packages.
+    // A package can contain sub packages.
+    
+    // by default: java.lang.* is imported to all the java files
+    // java.lang.* contains all the basic classes and interfaces
+    // like: String, System, Object, etc.
+
     }
 }
