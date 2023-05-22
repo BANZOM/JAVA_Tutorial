a2dab7ca9db8a3f3781bd2ef0eeb41c9dddcb08b
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-22 12:25:23 +0530

    AdvCalc without inherit

---
+++ InheritanceImplementation/AdvCalc.java
@@ -0,0 +1,11 @@
+package InheritanceImplementation;
+
+public class AdvCalc {
+    public int multi(int a, int b) {
+        return a * b;
+    }
+
+    public double div(int a, int b) {
+        return (double)a / b;
+    }
+}
--- InheritanceImplementation/Inheritance.java
+++ InheritanceImplementation/Inheritance.java
@@ -6,5 +6,9 @@ public class Inheritance {
         Calc c = new Calc();
         System.out.println(c.add(1, 2));
         System.out.println(c.subtract(1, 2));
+
+        AdvCalc ac = new AdvCalc();
+        System.out.println(ac.multi(1, 2));
+        System.out.println(ac.div(1, 2));
     }
 }
