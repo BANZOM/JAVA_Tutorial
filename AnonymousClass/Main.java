d04ab244e67a33c5fa75d266b5b4d7892fb5b53f
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-23 14:01:43 +0530

    Anonymous inner class

--- AnonymousClass/Main.java
+++ AnonymousClass/Main.java
@@ -8,7 +8,17 @@ class A{
 
 public class Main {
     public static void main(String[] args) {
-        A obj = new A();
-        obj.method1();
+        A obj1 = new A();
+        obj1.method1();
+
+        A obj2 = new A() // this is the anonymous class as it don't have name
+        {
+            void method1(){
+                System.out.println("in modified method1....");
+            }
+        };
+        obj2.method1();
+
+        
     }
 }
