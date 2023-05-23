9edb9809b78c604292660e8147fa6f51199ea85e
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-23 10:38:23 +0530

    A have a abstract method

--- AbstractImplementation/A.java
+++ AbstractImplementation/A.java
@@ -1,8 +1,10 @@
 package AbstractImplementation;
 
-public class A {
+public abstract class A {
     public void method1A() {
         System.out.println("this is in method 1A...");
     }
+
+    public abstract void method2A(); // making it abstract
 
 }
