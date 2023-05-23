82c8895cd17b1ada9cb55658a9ff651e8b0bcb7d
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-23 14:10:34 +0530

    Anonymous with Abstract Class

--- AbstractAnnonymousClass/Main.java
+++ AbstractAnnonymousClass/Main.java
@@ -7,6 +7,13 @@ abstract class A{
 
 public class Main {
     public static void main(String[] args) {
+        A obj = new A() {
+            void method1(){
+                System.out.println("in anonymous method1....");
+            }
+        };
+
+        obj.method1();
         
     }
 }
