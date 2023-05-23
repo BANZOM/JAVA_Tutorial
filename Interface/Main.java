9a5411ee799baf3c7d8f218e85116595a4debf1d
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-23 15:16:12 +0530

    if dev have desktop he can't code

--- Interface/Main.java
+++ Interface/Main.java
@@ -6,6 +6,12 @@ class Laptops{
     }
 }
 
+class Desktop{
+    void development() {
+        System.out.println("desktop hardware...");
+    }
+}
+
 class Developer{
     void coding(Laptops laptop) {
         System.out.println("Developer is coding...");
@@ -16,8 +22,10 @@ public class Main {
     public static void main(String[] args) {
         Developer dev1 = new Developer();
         Laptops lap1 = new Laptops();
+        Desktop desk1 = new Desktop();
 
         dev1.coding(lap1);
+        // dev1.coding(desk1);  not works
 
         
     }
