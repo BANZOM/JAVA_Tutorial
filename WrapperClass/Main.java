e6217e57ce53488fa342555b6b9dd0f37e142988
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-23 10:14:11 +0530

    in Code implemenatation

--- WrapperClass/Main.java
+++ WrapperClass/Main.java
@@ -3,6 +3,18 @@ package WrapperClass;
 public class Main {
     public static void main(String[] args) {
         
+        int n = 10;
+        Integer wn1 = n;  // autoboxing
+        Integer wn2 = new Integer(n); // boxing but deprecated
+        System.out.println(wn1);
+        System.out.println(wn2);
+
+        int pn1 = wn1.intValue(); // unboxing
+        int pn2 = wn2;   // auto unboxing
+        System.out.println(pn1);
+        System.out.println(pn2);
+
+
         
     }
 }
