c1e108c97f143f5c13b5d1e91b20a9e929bfdc6d
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-23 13:45:09 +0530

    Inner class

--- InnerClass/Main.java
+++ InnerClass/Main.java
@@ -5,11 +5,20 @@ class Outer{
     void show() {
         System.out.println("in Outer..");
     }
+
+    class Inner{
+        void display() {
+            System.out.println("in Inner..");
+        }
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
         Outer objO = new Outer();
         objO.show();
+
+        Outer.Inner objI = objO.new Inner();
+        objI.display();
     }
 }
