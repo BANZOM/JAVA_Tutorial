b191267997561f9def890a74a7f9139e7d8a0ac9
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-23 14:08:06 +0530

    Abstract Class

--- AbstractAnnonymousClass/Main.java
+++ AbstractAnnonymousClass/Main.java
@@ -1,15 +1,12 @@
 package AbstractAnnonymousClass;
 
-class A{
-    void method1(){
-        System.out.println("in default method1....");
-    }
+abstract class A{
+    abstract void method1();
 }
 
 
 public class Main {
     public static void main(String[] args) {
-        A obj1 = new A();
-        obj1.method1();
+        
     }
 }
