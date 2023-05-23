72a708520f939d46bb13acbdc828aec89298253c
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-23 15:07:27 +0530

    Basic Code

---
+++ Interface/Main.java
@@ -0,0 +1,14 @@
+package Interface;
+
+class A {
+    public void show() {
+        System.out.println("A");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        A a = new A();
+        a.show();
+    }
+}
