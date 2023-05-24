a1e9663b16c540b131c51656e043043f4ae4f395
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-25 00:05:06 +0530

    operations  on enums

--- Enums/Main1.java
+++ Enums/Main1.java
@@ -1,5 +1,25 @@
 package Enums;
 
+enum BasicEnum{
+    ONE,TWO,THREE,START,STOP,TERMINATE,FULL,EMPTY,ETC,UNKNOWN;
+}
 public class Main1 {
-    
+    public static void main(String[] args) {
+        BasicEnum e = BasicEnum.ONE;
+        System.out.println(e);
+
+        System.out.println(BasicEnum.TWO);
+        System.out.println(BasicEnum.THREE);
+        System.out.println(BasicEnum.START);
+        System.out.println(BasicEnum.STOP);
+        System.out.println(BasicEnum.TERMINATE);
+        System.out.println(BasicEnum.FULL);
+        System.out.println(BasicEnum.EMPTY);
+        System.out.println(BasicEnum.ETC);
+        System.out.println(BasicEnum.UNKNOWN);
+
+        System.out.println("\n\n\n");
+
+        BasicEnum[] x = BasicEnum.values(); // getting all enums in an array
+    }
 }
