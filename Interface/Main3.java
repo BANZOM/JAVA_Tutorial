d6450a049c41c7d3449f50f782ce4d08c99b443b
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-24 23:34:35 +0530

    Multiple implements

--- Interface/Main3.java
+++ Interface/Main3.java
@@ -12,6 +12,28 @@ interface Human{
     void think();
 }
 
+class Amain3 implements Animal,Human{
+    public void eat(){
+        System.out.println("Eating...");
+    }
+
+    public void sleep(){
+        System.out.println("Sleeping...");
+    }
+
+    public void think(){
+        System.out.println("Thinking...");
+    }
+}
+
 public class Main3 {
+    public static void main(String[] args) {
+        Amain3 obj = new Amain3();
+        obj.eat();
+        obj.sleep();
+        obj.think();
+        System.out.println("Average weight of human: " + Human.avgWeight);
+        System.out.println("Average weight of animal: " + Animal.avgWeight);
+    }
     
 }
