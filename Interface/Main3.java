468aaef8eacaed12c7dc85d09df7cbf68b716e4e
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-24 22:57:12 +0530

    More on interface

---
+++ Interface/Main3.java
@@ -0,0 +1,17 @@
+package Interface;
+
+interface Animal{
+    int avgWeight = 100;
+    void eat();
+    void sleep();
+}
+
+interface Human{
+    int avgWeight = 50;
+    void eat();
+    void think();
+}
+
+public class Main3 {
+    
+}
