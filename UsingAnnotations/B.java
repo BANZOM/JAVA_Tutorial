89df13165417da65ef57b99c5595ed14e07cd177
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-25 23:54:37 +0530

    using @Override

--- UsingAnnotations/B.java
+++ UsingAnnotations/B.java
@@ -1,5 +1,12 @@
 package UsingAnnotations;
 
 public class B extends A {
-    
+
+    @Override
+    void toBeOverride() {
+        // Generated using source action
+        System.out.println("Hello Universe");
+    }
+
+
 }
--- UsingAnnotations/Main1.java
+++ UsingAnnotations/Main1.java
@@ -2,6 +2,7 @@ package UsingAnnotations;
 
 public class Main1 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        B b = new B();
+        b.toBeOverride();
     }
 }
