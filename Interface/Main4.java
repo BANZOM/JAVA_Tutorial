965ac47d0777f7908dac147cff60cb8851a6395b
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-26 00:04:36 +0530

    FA implements

--- Interface/Main4.java
+++ Interface/Main4.java
@@ -5,9 +5,14 @@ interface FI{
     void development();
     // void show(); Functional interface only have one method
 }
-
+class FA implements FI {
+    public void development(){
+        System.out.println("functional interface...");
+    }
+}
 public class Main4 {
     public static void main(String[] args) {
-        
+        FI x = new FA();
+        x.development();
     }
 }
