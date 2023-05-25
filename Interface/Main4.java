32444dbb71f29ed38ab289f55cc8797576b99dd7
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-26 00:05:48 +0530

    FI through anonymous method

--- Interface/Main4.java
+++ Interface/Main4.java
@@ -5,14 +5,19 @@ interface FI{
     void development();
     // void show(); Functional interface only have one method
 }
-class FA implements FI {
-    public void development(){
-        System.out.println("functional interface...");
-    }
-}
+// class FA implements FI {
+//     public void development(){
+//         System.out.println("functional interface...");
+//     }
+// }
 public class Main4 {
     public static void main(String[] args) {
-        FI x = new FA();
+        FI x = new FI(){
+            @Override
+            public void development() {
+                System.out.println("functional interface through anonymous method...");
+            }
+        };
         x.development();
     }
 }
