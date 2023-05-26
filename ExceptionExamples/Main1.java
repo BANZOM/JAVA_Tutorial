f1c9aa78eda8bcf492e23d8587f9622c37b705ed
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-26 16:00:23 +0530

    Try - Catch

--- ExceptionExamples/Main1.java
+++ ExceptionExamples/Main1.java
@@ -4,7 +4,13 @@ public class Main1 {
     public static void main(String[] args) {
         
         for (int i = -10; i <= 10; i++) {
-            System.out.println(10/i);
+            try {
+                System.out.println(10/i);
+            } catch (Exception e) {
+                System.out.println("Something went wrong when i = "+ i);
+            }
         }
+
+        System.out.println("End of program");
     }
 }
