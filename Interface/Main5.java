9351a367943a7d9750f5bcc9fbf2c19d7897eb59
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-26 14:11:54 +0530

    Initiated sample FI

---
+++ Interface/Main5.java
@@ -0,0 +1,15 @@
+package Interface;
+/*
+ * The Lamda Expression (->) used with functional interface
+ */
+
+@FunctionalInterface
+interface LE{
+    void display();
+}
+
+public class Main5 {
+    public static void main(String[] args) {
+        
+    }
+}
