85a430c43f2f928c3bbf6e27e947530e0735cd9c
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-26 14:18:48 +0530

    Lamda Expression with variables

--- Interface/Main5.java
+++ Interface/Main5.java
@@ -4,18 +4,28 @@ package Interface;
  */
 
 @FunctionalInterface
-interface LE{
+interface LE1{
     void display();
+}
+
+@FunctionalInterface
+interface LE2{
+    void sumOfTwo(int x, int y);
 }
 
 public class Main5 {
     public static void main(String[] args) {
 
-        LE x = () -> {
+        LE1 x = () -> {
             System.out.println("printing with the help of Lamda Expression...");
         };
 
         x.display();
         
+        LE2 y = (int i, int j) -> {
+            System.out.println("Sum of two numbers: " + (i+j));
+        };
+
+        y.sumOfTwo(10,20);
     }
 }
