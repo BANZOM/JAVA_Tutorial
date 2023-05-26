5e0b8e75a6daa69c7b314dcaa12deea0ffd1b4e8
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-26 15:59:02 +0530

    Exception when dividing by 0

---
+++ ExceptionExamples/Main1.java
@@ -0,0 +1,10 @@
+package ExceptionExamples;
+
+public class Main1 {
+    public static void main(String[] args) {
+        
+        for (int i = -10; i <= 10; i++) {
+            System.out.println(10/i);
+        }
+    }
+}
