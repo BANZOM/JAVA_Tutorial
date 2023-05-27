ec00ae8d5c80461550b682ac73aa5027430545d3
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-27 18:26:59 +0530

    sample code having exceptions

--- ExceptionExamples/Main4.java
+++ ExceptionExamples/Main4.java
@@ -5,8 +5,25 @@ package ExceptionExamples;
  */
 
 
+class main4A {
+    void methodA1() {
+        System.out.println(10/0);
+    }
+}
+
+class main4B {
+    void methodB1() {
+        int[] arr = new int[3];
+        System.out.println(arr[3]);
+    }
+}
 public class Main4 {
     public static void main(String[] args) {
+        main4A a = new main4A();
+        a.methodA1();
+
+        main4B b = new main4B();
+        b.methodB1();
         
     }
 }
