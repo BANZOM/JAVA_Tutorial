b415d37b504a3e5106769a24b012369f58c279dd
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-27 19:50:34 +0530

    Reading from Scanner

--- InputTaking/Main1.java
+++ InputTaking/Main1.java
@@ -2,6 +2,7 @@ package InputTaking;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /*
  * Taking the Input
@@ -11,13 +12,22 @@ import java.io.InputStreamReader;
 public class Main1 {
     public static void main(String[] args) throws IOException {
         
-        InputStreamReader in = new InputStreamReader(System.in);
-        BufferedReader rf = new BufferedReader(in);
+        // InputStreamReader in = new InputStreamReader(System.in);
+        // BufferedReader rf = new BufferedReader(in);
 
-        int i = Integer.parseInt(rf.readLine()); // returns String
-        System.out.println("Given input: "+ i);
+        // int i = Integer.parseInt(rf.readLine()); // returns String
+        // System.out.println("Given input: "+ i);
 
 
-        rf.close();
+        // rf.close();
+
+
+        // scanner
+        Scanner rS = new Scanner(System.in);
+        String i = rS.nextLine();
+        int j = rS.nextInt();
+        System.out.println("Given input: "+ i +" " +j); 
+
+        rS.close();
     }
 }
