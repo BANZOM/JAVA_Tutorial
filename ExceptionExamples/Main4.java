235fc94406ab2e68a9b50642229df343fca9421b
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-27 18:36:31 +0530

    Ducking the exception with classes

--- ExceptionExamples/Main4.java
+++ ExceptionExamples/Main4.java
@@ -17,21 +17,28 @@ class main4B {
         System.out.println(arr[3]);
     }
 }
-public class Main4 {
-    public static void main(String[] args) {
+
+class main4C{
+
+    void methodC1() throws Exception {
         main4A a = new main4A();
         main4B b = new main4B();
-        
-        try {
-            a.methodA1();
-        } catch (Exception e) {
-            System.out.println("Exception is handled by ducking " + e);
-        }
-        
-        try {
-            b.methodB1();
-        } catch (Exception e) {
-            System.out.println("Exception is handled by ducking " + e);
-        }
+        a.methodA1();
+        b.methodB1();
     }
+}
+
+public class Main4 {
+    public static void main(String[] args) {
+        
+        main4C c = new main4C();
+
+        try {
+            c.methodC1();
+        } catch (Exception e) {
+            System.out.println("Exception is handled by ducking " + e);
+        }
+        
+        System.out.println("End of program");
+    }  
 }
