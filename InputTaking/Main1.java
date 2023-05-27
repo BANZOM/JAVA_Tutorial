b32504a48a47296debc687a60872e76fe0f822d7
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-27 19:39:14 +0530

    BufferReader (complex, old)

--- InputTaking/Main1.java
+++ InputTaking/Main1.java
@@ -1,11 +1,20 @@
 package InputTaking;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /*
  * Taking the Input
  * 1. using BufferReader
  * 2. using Scanner
  */
 public class Main1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader rf = new BufferedReader(in);
+
+        int i = rf.read(); // returns ASCII VAlues
+        System.out.println(i); 
     }
 }
