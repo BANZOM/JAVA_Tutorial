ea03dd837798f5bb0a148c8a94ab5018884fc579
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-27 18:02:49 +0530

    Custom Exception with message

--- ExceptionExamples/Main3.java
+++ ExceptionExamples/Main3.java
@@ -4,8 +4,30 @@ package ExceptionExamples;
  * Generating Custom Exceptions
  */
 
+class AdityaBuildAnException extends Exception {
+    public AdityaBuildAnException(String customMessage) {
+        super(customMessage);
+    }
+}
+
 public class Main3 {
     public static void main(String[] args) {
-        
+        for (int i = -10; i <= 10; i++) {
+            int n;
+            try {
+                n = 10/i;
+                if (n == 1) {
+                    throw new AdityaBuildAnException("custom exception throwed");
+                }
+            } catch (AdityaBuildAnException e) {
+                n = 10/(i+1);
+                System.out.println(e);
+            }
+            catch (Exception e) {
+                n = i;
+                System.out.println("Something went wrong when i = "+ i);
+            }
+            System.out.println(n);
+        }
     }
 }
