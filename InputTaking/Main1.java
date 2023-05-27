e7d5d4e60cdb41d0a29dfc3e36d39cdc01bb3b71
BANZOM <aditya.2125it1040@kiet.edu>
2023-05-27 19:31:27 +0530

    input init

---
+++ InputTaking/Main1.java
@@ -0,0 +1,11 @@
+package InputTaking;
+/*
+ * Taking the Input
+ * 1. using BufferReader
+ * 2. using Scanner
+ */
+public class Main1 {
+    public static void main(String[] args) {
+        
+    }
+}
