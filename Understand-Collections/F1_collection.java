be09d5a443534dd262fb150eaf84f1738503832b
banzom <aditya.panwar.2208@gmail.com>
2023-08-20 18:16:53 +0530

    modified:   Understand-Collections/F1_collection.java

--- Understand-Collections/F1_collection.java
+++ Understand-Collections/F1_collection.java
@@ -1,5 +1,9 @@
 class F1_collection {
     public static void main(String[] args) {
-        
+        /*
+         * Collection -> Interface
+         * Collections -> Class
+         * Collection API -> concept
+         */
     }
 }
