88df93d76b654b72ce1fde77f640992aee2509fb
banzom <aditya.panwar.2208@gmail.com>
2023-08-20 18:18:48 +0530

    modified:   Understand-Collections/F1_collection.java

--- Understand-Collections/F1_collection.java
+++ Understand-Collections/F1_collection.java
@@ -1,9 +1,17 @@
+import java.util.Collection;
+
 class F1_collection {
     public static void main(String[] args) {
         /*
          * Collection -> Interface
          * Collections -> Class
          * Collection API -> concept
-         */
+        */
+
+        Collection c = new Collection<E>() {
+            // since it's a interface so needed to implement all the methods
+        };
+
+        
     }
 }
