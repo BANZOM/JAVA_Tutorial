02c46365b61ed4312ae8feac16e7634670c59571
banzom <aditya.panwar.2208@gmail.com>
2023-08-20 19:01:13 +0530

    Using Iterator to print

--- Understand-Collections/F3_Set.java
+++ Understand-Collections/F3_Set.java
@@ -1,5 +1,6 @@
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class F3_Set {
     public static void main(String[] args) {
@@ -7,16 +8,24 @@ public class F3_Set {
         // Instead of using the class HashSet, if we will use the TreeSet, it will give values in sorted order.
         Set <Integer> set = new HashSet<Integer>();
 
-        set.add(1);
         set.add(2);
-        set.add(3);
+        set.add(1);
         set.add(4);
+        set.add(3);
         set.add(2);
         set.add(3);
 
         // It doesn't store the duplicates
         System.out.println(set);
         System.out.println(set.size());
+        System.out.println("------------");
+
+        // Printing through Iterator
+        Iterator<Integer> I = set.iterator();
+
+        while (I.hasNext()) {
+            System.out.println(I.next());
+        }
 
     }
 }
