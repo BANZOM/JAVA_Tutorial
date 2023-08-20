6190a6a015afbb92240a8d7d856472ea7fc2d723
banzom <aditya.panwar.2208@gmail.com>
2023-08-20 18:38:06 +0530

    Not Recommended

--- Understand-Collections/F1_collection.java
+++ Understand-Collections/F1_collection.java
@@ -10,14 +10,16 @@ class F1_collection {
         */
 
         // Collection only works with objects not primitive types i.e int,etc;
-        Collection<Integer> c = new ArrayList<Integer>();
+        Collection c = new ArrayList();
         c.add(1);
         c.add(2);
         c.add(3);
         c.add(4);
 
-        for (Integer i : c) {
-            System.out.println(i);
+        // works but highly prone to runtime exceptions
+        for (Object i : c) {
+            int num = (int) i;
+            System.out.println(num);
         }
         
 
