85e63dbc3b2d35612083b0c5eee653cbbce32ab9
banzom <aditya.panwar.2208@gmail.com>
2023-08-20 18:46:53 +0530

    List

--- Understand-Collections/F2_ArrayList.java
+++ Understand-Collections/F2_ArrayList.java
@@ -1,16 +1,25 @@
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 
 public class F2_ArrayList {
     public static void main(String[] args) {
         ArrayList<Integer> al = new ArrayList<>();
 
-        al.add(1);
-        al.add(2);
-        al.add(3);
+        // al.add(1);
+        // al.add(2);
+        // al.add(3);
+        // for (Integer i : al) {
+        //     System.out.println(i);
+        // }
 
-        for (Integer i : al) {
-            System.out.println(i);
-        }
+
+        // List have many extra useful methods 
+        List<String> l = new ArrayList<>();
+
+        l.add(0, "null");  // [null]
+        l.add(1, "null");  // [null, null]
+        l.add(1, "Aditya");  // [null, Aditya, null]
+
+        System.out.println(l); // [null, Aditya, null]
     }
 }
