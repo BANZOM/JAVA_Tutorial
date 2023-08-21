3262e15fc4a1728a6d05131d6b0153fab78c1eab
banzom <aditya.panwar.2208@gmail.com>
2023-08-21 12:43:18 +0530

    new file:   Understand-Collections/F5_Comparator.java

---
+++ Understand-Collections/F5_Comparator.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class F5_Comparator {
+    public static void main(String[] args) {
+        
+        List<Integer> list = new ArrayList<>();
+        list.add(12);
+        list.add(26);
+        list.add(3);
+        list.add(49);
+        list.add(35);
+
+        System.out.println(list);
+        
+    }
+}
