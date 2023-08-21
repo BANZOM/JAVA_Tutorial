78dbfbc30b1327b4965409cb3276f62c1dabe00a
banzom <aditya.panwar.2208@gmail.com>
2023-08-21 13:28:56 +0530

    class

--- Understand-Collections/F6_Comparable.java
+++ Understand-Collections/F6_Comparable.java
@@ -1,6 +1,33 @@
+import java.util.*;
+
+class Students {
+    private int roll;
+    private String name;
+
+    Students(int roll, String name) {
+        this.roll = roll;
+        this.name = name;
+    }
+
+    public String toString() {
+        return roll + " : " + name;
+    }
+
+
+}
+
 public class F6_Comparable {
     public static void main(String[] args) {
         
         // Here we'll try to use the Comparable interface for our own classes 
+        List <Students> list = new ArrayList<>();
+
+        list.add(new Students(1, "Aditya"));
+        list.add(new Students(2, "Rahul"));
+        list.add(new Students(3, "Sonam"));
+        list.add(new Students(4, "Amit"));
+        list.add(new Students(5, "Sun"));
+
+        System.out.println(list);
     }
 }
