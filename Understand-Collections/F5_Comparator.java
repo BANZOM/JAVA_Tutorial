5744609accfc201e0f6c9a7a5db160c2dd512fc2
banzom <aditya.panwar.2208@gmail.com>
2023-08-21 12:53:09 +0530

    swap based on last digit

--- Understand-Collections/F5_Comparator.java
+++ Understand-Collections/F5_Comparator.java
@@ -3,6 +3,25 @@ import java.util.*;
 public class F5_Comparator {
     public static void main(String[] args) {
 
+        Comparator<Integer> comp = new Comparator<Integer>() {
+
+            /**
+             * + * Compares two Integers based on their remainder when divided by 10.
+             * + *
+             * + * @param o1 the first Integer to be compared
+             * + * @param o2 the second Integer to be compared
+             * + * @return a positive integer if o1%10 > o2%10, otherwise a negative integer
+             * +
+             */
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (o1 % 10 > o2 % 10) {
+                    return 1;
+                }
+                return -1;
+            }
+        };
+
         List<Integer> list = new ArrayList<>();
         list.add(12);
         list.add(26);
@@ -14,7 +33,10 @@ public class F5_Comparator {
 
         // Sorting the list
         Collections.sort(list);
+        System.out.println(list);
 
+        // Sorting of list with respect to own logic of sort
+        Collections.sort(list, comp);
         System.out.println(list);
     }
 }
