6d4ff4c6bcf5da2395882582bb1cb454ab87c20a
banzom <aditya.panwar.2208@gmail.com>
2023-08-21 14:38:56 +0530

    reduced comparator

--- Understand-Collections/F6_Comparable.java
+++ Understand-Collections/F6_Comparable.java
@@ -28,15 +28,12 @@ class Students implements Comparable<Students> {
 public class F6_Comparable {
     public static void main(String[] args) {
         
-        Comparator<Students> comp = new Comparator<Students>() {
-            @Override
-            public int compare(Students o1, Students o2) {
+        Comparator<Students> comp = (Students o1, Students o2) -> {
                 if (o1.name.length() > o2.name.length()) {
                     return 1;
                 }
                 return -1;
-            }
-        };
+            };
 
         // Here we'll try to use the Comparable interface for our own classes 
         List <Students> list = new ArrayList<>();
