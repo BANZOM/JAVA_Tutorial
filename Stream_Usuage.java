b7f8a422f5137e22a4bfa12877b15cf5489a6628
banzom <aditya.panwar.2208@gmail.com>
2023-08-21 17:09:19 +0530

    filter

--- Stream_Usuage.java
+++ Stream_Usuage.java
@@ -5,11 +5,14 @@ public class Stream_Usuage {
     public static void main(String[] args) {
         // Understanding Stream
 
-        List<Integer> list = Arrays.asList(1,3,2,4,5,3,6,4,3,6,32,53,12,43,52,2);
+        List<Integer> list = Arrays.asList(1, 3, 2, 4, 5, 3, 6, 4, 3, 6, 32, 53, 12, 43, 52, 2);
 
         Stream<Integer> stream = list.stream();
-        System.out.println(stream.count());
-        // System.out.println(stream.findAny());  // error due to it cannot be reused
+        // using stream to find filter only even nums;
+        Stream<Integer> stream1 = stream.filter(i -> i % 2 == 0);
+        stream1.forEach(i -> System.out.print(i + ", "));
+
         
+
     }
 }
