b5f717d2d2bfabe813265c1ddcac2fbe340c3767
banzom <aditya.panwar.2208@gmail.com>
2023-08-21 16:52:57 +0530

    modified:   Stream_Usuage.java

--- Stream_Usuage.java
+++ Stream_Usuage.java
@@ -1,5 +1,15 @@
+import java.util.*;
+import java.util.stream.Stream;
+
 public class Stream_Usuage {
     public static void main(String[] args) {
         // Understanding Stream
+
+        List<Integer> list = Arrays.asList(1,3,2,4,5,3,6,4,3,6,32,53,12,43,52,2);
+
+        Stream<Integer> stream = list.stream();
+        System.out.println(stream.count());
+        // System.out.println(stream.findAny());  // error due to it cannot be reused
+        
     }
 }
