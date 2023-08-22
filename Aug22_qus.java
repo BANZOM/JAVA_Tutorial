db694bc587d24ea5b4db0bb6708fba03a7e856a3
banzom <aditya.panwar.2208@gmail.com>
2023-08-22 09:34:56 +0530

    modified:   Aug22_qus.java

--- Aug22_qus.java
+++ Aug22_qus.java
@@ -1,13 +1,14 @@
 import java.util.*;
 
-import javax.swing.tree.ExpandVetoException;
-
 class Aug22_qus {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.print("Enter: ");
         int input = in.nextInt();
 
-        Aug22_qus_logic a = new Aug22_qus_logic(4);
+        Aug22_qus_logic a = new Aug22_qus_logic(input);
+
+        System.out.println("Output: " + a.output());
     }
 }
 
@@ -15,7 +16,18 @@ class Aug22_qus_logic {
     int input;
 
     Aug22_qus_logic(int i) {
+        if (i > 1 || i < 0) {
+            throw new RuntimeException("Enter 0 or 1 only");
+        }
         this.input = i;
         System.out.println("Done");
+    }
+
+    int output() {
+        try {
+            return 0 / this.input;
+        } catch (Exception e) {
+            return 1;
+        }
     }
 }
