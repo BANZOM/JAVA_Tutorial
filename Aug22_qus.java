471ec99349bc1582513e43b20bef1cb259da43ae
banzom <aditya.panwar.2208@gmail.com>
2023-08-22 09:14:10 +0530

    new file:   Aug22_qus.java

---
+++ Aug22_qus.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+import javax.swing.tree.ExpandVetoException;
+
+class Aug22_qus {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int input = in.nextInt();
+
+        Aug22_qus_logic a = new Aug22_qus_logic(4);
+    }
+}
+
+class Aug22_qus_logic {
+    int input;
+
+    Aug22_qus_logic(int i) {
+        this.input = i;
+        System.out.println("Done");
+    }
+}
