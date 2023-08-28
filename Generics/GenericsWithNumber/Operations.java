d6127ea84a599d869281a9a1c674821ed0f6e102
BANZOM <aditya.panwar.2208@gmail.com>
2023-08-28 21:21:48 +0530

    bounded type sort method

--- Generics/GenericsWithNumber/Operations.java
+++ Generics/GenericsWithNumber/Operations.java
@@ -1,12 +1,18 @@
 package Generics.GenericsWithNumber;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
-public class Operations <N extends Number> {
+public class Operations <N extends Number & Comparable<N>> {
     List<N> list;
 
     public Operations(List<N> list) {
         this.list = list;
     }
-    
+
+    public List sort() {
+        Collections.sort(this.list);
+        return this.list;
+    }
 }
