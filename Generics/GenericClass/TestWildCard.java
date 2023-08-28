5733b6aef9e5e50a23d464db8214c71479bac9e9
BANZOM <aditya.panwar.2208@gmail.com>
2023-08-29 01:18:14 +0530

    Commented lines -> cannot use wildcards

---
+++ Generics/GenericClass/TestWildCard.java
@@ -0,0 +1,10 @@
+package Generics.GenericClass;
+
+// public class TestWildCard extends Box<?> 
+public class TestWildCard {
+    public static void main(String[] args) {
+        // new Box<?>();
+
+        // var box = BoxUtil.<?>getEmptyBox();
+    }
+}
