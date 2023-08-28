fa7da48c3b6a8028b5feb8ffd07994cd58b7be8b
BANZOM <aditya.panwar.2208@gmail.com>
2023-08-28 13:55:14 +0530

    new file:   Generics/With_Generic.java

---
+++ Generics/With_Generic.java
@@ -0,0 +1,18 @@
+package Generics;
+import java.util.*;
+
+public class With_Generic {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+
+        list.add("A");
+        list.add(new String("B"));
+
+        someOperation(list);
+    }
+
+    private static void someOperation(List<String> list) {
+        String s = list.get(0);
+        System.out.println(s);
+    }
+}
