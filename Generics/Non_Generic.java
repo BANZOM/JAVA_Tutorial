e9062c840f9ce2083ea0fef16dbfc4a511d08595
BANZOM <aditya.panwar.2208@gmail.com>
2023-08-28 13:40:30 +0530

    new file:   Generics/Non_Generic.java

---
+++ Generics/Non_Generic.java
@@ -0,0 +1,20 @@
+package Generics;
+import java.util.*;
+public class Non_Generic {
+    public static void main(String[] args) {
+        List list = new ArrayList();
+
+        list.add("A");
+        
+        String value = new String("B");
+        list.add(value);
+
+        someOperation(list);
+        
+    }
+
+    private static void someOperation(List list) {
+        String s = (String) list.get(0);
+        System.out.println(s);
+    }
+}
