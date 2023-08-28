472506b916d29376ac8fd5363ca63878c9ff0b66
BANZOM <aditya.panwar.2208@gmail.com>
2023-08-28 12:49:11 +0530

    modified:   Primitive_Casting.java

--- Primitive_Casting.java
+++ Primitive_Casting.java
@@ -1,10 +1,13 @@
 public class Primitive_Casting {
     public static void main(String[] args) {
-        float f = 1000.5f;
+        float f = 234.56f;
         int i = (int)f;
         System.out.println(i);
 
         short s = (short)f;
         System.out.println(s);
+
+        byte b = (byte)f;
+        System.out.println(b);
     }
 }
