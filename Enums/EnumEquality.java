040abca68c93f7c1924c27add434c858d1244995
BANZOM <aditya.panwar.2208@gmail.com>
2023-08-30 11:13:06 +0530

    Equality of enums

---
+++ Enums/EnumEquality.java
@@ -0,0 +1,11 @@
+package Enums;
+
+public class EnumEquality {
+    public static void main(String[] args) {
+        BasicEnum a = BasicEnum.START;
+        BasicEnum b = BasicEnum.START;
+
+        if (a == b)
+            System.out.println("==");
+    }
+}
