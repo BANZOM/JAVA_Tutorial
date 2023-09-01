d5d5cee8ccc3e101049f62c31b4117e9e7f5bd79
BANZOM <aditya.panwar.2208@gmail.com>
2023-09-01 22:09:09 +0530

    witheffect using "-ea" while executing

--- ExceptionExamples/UnderstandingAssert.java
+++ ExceptionExamples/UnderstandingAssert.java
@@ -4,6 +4,8 @@ package ExceptionExamples;
  */
 public class UnderstandingAssert {
     public static void main(String[] args) {
-        
+        int a = 15;
+        assert a >= 18: "Assertion failed";
+        System.out.println("Assertion passed");
     }
 }
