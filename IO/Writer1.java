e19ad40d03651b8fd6263555cefe725027d04236
BANZOM <aditya.panwar.2208@gmail.com>
2023-09-02 15:52:00 +0530

    modified:   IO/Writer1.java

--- IO/Writer1.java
+++ IO/Writer1.java
@@ -4,8 +4,19 @@ import java.io.IOException;
 
 public class Writer1 {
     public static void main(String[] args) {
+
+        // Code snipped to create new file
         File file = new File("IO/fileWrite1.txt");
         System.out.println(file.exists());
+
+        try {
+            boolean newFile = file.createNewFile();
+            // System.out.println(newFile);
+
+        }
+        catch (IOException e) {
+            System.out.println("Unable to create file");
+        }
 
     }
 }
