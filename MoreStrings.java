dfd1c64682702ac31ec50aa92098da869b8204b3
BANZOM <aditya.panwar.2208@gmail.com>
2023-09-02 11:02:31 +0530

    modified:   MoreStrings.java

--- MoreStrings.java
+++ MoreStrings.java
@@ -3,7 +3,12 @@ public class MoreStrings {
         String s1 = "Hello";
         String s2 = s1;
 
-        System.out.println(s1);
-        System.out.println(s2);
+        System.out.println(s1); // "Hello"
+        System.out.println(s2); // "Hello"
+
+        s1 = s1.concat("World");
+
+        System.out.println(s1); // "HelloWorld"
+        System.out.println(s2); // "Hello"
     }
 }
