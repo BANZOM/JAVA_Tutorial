5f1fbfa5d74950411c21454fb8b6463161e7c2ae
BANZOM <aditya.panwar.2208@gmail.com>
2023-09-02 11:01:17 +0530

    new file:   MoreStrings.java

---
+++ MoreStrings.java
@@ -0,0 +1,9 @@
+public class MoreStrings {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = s1;
+
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
