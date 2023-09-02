034c94146b47477b72f6298a8f70fce3f20b8617
BANZOM <aditya.panwar.2208@gmail.com>
2023-09-02 16:25:50 +0530

    modified:   IO/ReadingFile.java

--- IO/ReadingFile.java
+++ IO/ReadingFile.java
@@ -1,7 +1,19 @@
 package IO;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class ReadingFile {
     public static void main(String[] args) {
-        
+        try {
+            Scanner scanner = new Scanner(new File("IO/fileWrite1.txt"));
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
