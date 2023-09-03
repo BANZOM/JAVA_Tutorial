8f7faa809dc6cb047193a48e38551c335bb37739
BANZOM <aditya.panwar.2208@gmail.com>
2023-09-03 14:46:57 +0530

    modified:   IO/ConsoleHandsOn.java

--- IO/ConsoleHandsOn.java
+++ IO/ConsoleHandsOn.java
@@ -12,6 +12,12 @@ public class ConsoleHandsOn {
         for (char c: ch) {
             console.format("%s ", c);  // this is printing that password characters
         }
+        console.format("\n");
+
+
+        String str = console.readLine("Enter: ");  // this allows the user to enter any text, it is visible
+        console.format("Output is %s\n", str);  // this is just an alternative to System.out.println
+        
         
     }
 }
