ca0a6e59a9bdd41c287cbbfefc5695d9ae7f37e3
BANZOM <aditya.panwar.2208@gmail.com>
2023-09-03 14:49:17 +0530

    modified:   IO/ConsoleHandsOn.java

--- IO/ConsoleHandsOn.java
+++ IO/ConsoleHandsOn.java
@@ -4,7 +4,7 @@ import java.io.Console;
 
 public class ConsoleHandsOn {
     public static void main(String[] args) {
-        Console console = System.console();
+        Console console = System.console();  // this creates a console object which is futher used for reading and writing
         char[] ch;
 
         ch = console.readPassword("%s","Enter Password: ");  // this allows the user to enter password that is hidden while typing
