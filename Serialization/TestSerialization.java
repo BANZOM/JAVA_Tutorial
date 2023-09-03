23e99c1bafa89d4d361cdad3b96312fec5e896f5
BANZOM <aditya.panwar.2208@gmail.com>
2023-09-03 22:18:16 +0530

    modified:   Serialization/TestSerialization.java

--- Serialization/TestSerialization.java
+++ Serialization/TestSerialization.java
@@ -4,6 +4,6 @@ public class TestSerialization implements Serializable {
     int a = 14;
     transient int b = 15;  // transient are not serialized
     Test2Serialization c = new Test2Serialization(16); // this class needs to be serialized 
-    transient Test3Serialization d = new Test3Serialization(17); // let suppose this cannot be serialized
+    transient Test3Serialization d = new Test3Serialization(17); // let suppose this cannot be serialized, then we make it transient
 
 }
