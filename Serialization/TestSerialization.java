424a0b01aacb28f9d39477aad2a3f6aaffbfad0f
BANZOM <aditya.panwar.2208@gmail.com>
2023-09-03 20:55:59 +0530

    transient - not serialized

--- Serialization/TestSerialization.java
+++ Serialization/TestSerialization.java
@@ -2,5 +2,5 @@ package Serialization;
 import java.io.Serializable;
 public class TestSerialization implements Serializable {
     int a = 14;
-    int b = 15;
+    transient int b = 15;  // transient are not serialized
 }
