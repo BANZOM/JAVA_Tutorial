a48f8fde1de9bb6df3df1e90d88ae8567472f0dd
BANZOM <aditya.panwar.2208@gmail.com>
2023-09-04 08:11:29 +0530

    new file:   DateClass.java

---
+++ DateClass.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class DateClass {
+    public static void main(String[] args) {
+        Date d = new Date();
+        String dateAsString = d.toString();
+
+        System.out.println("Date as String: " + dateAsString);
+    }
+}
