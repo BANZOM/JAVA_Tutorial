414606d8783c4d8d584ae429101d9d29a55a3af6
BANZOM <aditya.panwar.2208@gmail.com>
2023-09-04 08:21:07 +0530

    new file:   CalenderClass.java

---
+++ CalenderClass.java
@@ -0,0 +1,12 @@
+import java.util.Calendar;
+public class CalenderClass {
+    public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        System.out.println(String.format("%d/%d/%d", day, month, year));
+    }
+}
