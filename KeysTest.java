02de819ca8f6f97f92112083a9ac759ea414392b
BANZOM <aditya.panwar.2208@gmail.com>
2023-09-11 21:40:58 +0530

    modified:   KeysTest.java

--- KeysTest.java
+++ KeysTest.java
@@ -4,14 +4,16 @@ public class KeysTest {
     public static void main(String[] args) {
         
         TreeMap<String, String> treeMap = new TreeMap<>();
-        treeMap.put("one", "1");
-        treeMap.put("two", "2");
-        treeMap.put("three", "3");
-        treeMap.put("four", "4");
-        treeMap.put("five", "5");
+        treeMap.put("1","one");
+        treeMap.put("2","two");
+        treeMap.put("3","three");
+        treeMap.put("4","four");
+        treeMap.put("5","five");
+        treeMap.put("6","six");
 
         System.out.println(treeMap.keySet());
         System.out.println(treeMap.values());
 
+        System.out.println(treeMap.higherKey("3"));
     }
 }
